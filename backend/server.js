ee30b7d28516c341f43bc4eb7d73925b908657be
hpnavadiya <140366191+hpnavadiya@users.noreply.github.com>
2024-02-13 17:12:10 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 
+const path = require("path");
+
 dotenv.config();
 
 connectDB();
@@ -22,12 +24,21 @@ cloudinary.config({
 
 // --------------------------deployment------------------------------
 
-// config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "config/.env",
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
   });
 }
+
 
 // --------------------------deployment------------------------------
 
